21dcef6ba71a9c6cd37d74b483da743ded97f5af
one814469-ctrl <one814469@gmail.com>
2025-11-19 02:33:47 +0400

    Automated file deletion via n8n

--- src/router.jsx
+++
@@ -1,8 +0,0 @@
-// src/router.jsx
-import epicsData from './data/epics.json';
-
-// Dynamically generate routes based on epicsData
-export const appRoutes = epicsData.map(epic => ({
-  path: `/epic/${epic.title.toLowerCase().replace(/ /g, '-')}`,
-  epic: epic, // Pass the entire epic object to the route element
-}));
