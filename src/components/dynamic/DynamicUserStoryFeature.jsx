b8eabe843c756fb4c19c672c21271e457aa1b728
one814469-ctrl <one814469@gmail.com>
2025-11-19 10:00:38 +0400

    Add src/components/dynamic/DynamicUserStoryFeature.jsx

---
+++ src/components/dynamic/DynamicUserStoryFeature.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import UserStoryBlock from '../UserStoryBlock';
+
+/**
+ * DynamicUserStoryFeature
+ * Renders a single user story's block dynamically.
+ * This file is intended to be used by EpicPage or Epic route to render each story.
+ */
+const DynamicUserStoryFeature = ({ story, applicationData, onUpdateApplication, onLogin, isAuthenticated }) => {
+  if (!story) return null;
+
+  return (
+    <div className="dynamic-user-story">
+      <UserStoryBlock
+        story={story}
+        applicationData={applicationData}
+        onUpdateApplication={onUpdateApplication}
+        onLogin={onLogin}
+        isAuthenticated={isAuthenticated}
+      />
+    </div>
+  );
+};
+
+export default DynamicUserStoryFeature;
