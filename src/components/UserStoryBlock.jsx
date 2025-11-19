7f1d48efe8f9ebfb4a51a4c6d017e1fcb33e83f2
one814469-ctrl <one814469@gmail.com>
2025-11-19 10:49:18 +0400

    Add src/components/UserStoryBlock.jsx

---
+++ src/components/UserStoryBlock.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Card from './ui/Card';
+import DynamicTaskFeature from './dynamic/DynamicTaskFeature';
+
+const UserStoryBlock = ({ story, applicationData, onUpdateApplication, onLogin, isAuthenticated }) => {
+  return (
+    <Card>
+      <h3>{story.title}</h3>
+      <p>{story.description}</p>
+      <div className="user-story-tasks">
+        {story.tasks.map((task, index) => (
+          <div key={index} className="task-item mt-4 p-4 border-dashed">
+            <h4>Task: {task.title}</h4>
+            <p>{task.description}</p>
+            <DynamicTaskFeature
+              task={task}
+              applicationData={applicationData}
+              onUpdateApplication={onUpdateApplication}
+              onLogin={onLogin}
+              isAuthenticated={isAuthenticated}
+            />
+          </div>
+        ))}
+      </div>
+    </Card>  );
+};
+
+export default UserStoryBlock;
