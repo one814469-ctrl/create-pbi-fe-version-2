8e3f595009e4661cff37c67fc0d51ec3d67fe98a
one814469-ctrl <one814469@gmail.com>
2025-11-20 14:04:23 +0400

    Automated file deletion via n8n

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="footer">
-      <p>&copy; 2024 Swan Mauritius Loan Portal. All rights reserved.</p>
-    </footer>
-  )
-}
-
-export default Footer
