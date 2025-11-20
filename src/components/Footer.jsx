7fd278b4be05e1ced368d912fe58ce8e04d30cb2
one814469-ctrl <one814469@gmail.com>
2025-11-20 13:14:16 +0400

    Add src/components/Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>&copy; 2024 Swan Mauritius Loan Portal. All rights reserved.</p>
+    </footer>
+  )
+}
+
+export default Footer
